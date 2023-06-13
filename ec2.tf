c7c82da910c78e0cf03bd189872111f905911698
Rajesh Jagarlamudi <jagarlamudirajesh34@gmail.com>
2023-06-13 03:57:09 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,11 +1,47 @@
 provider "aws" {
  profile = "default"
- region = "ap-south-1"
+ region = "us-east-1"
 }
 
-resource "aws_instance" "myec2" {
-  ami = "ami-022d03f649d12a49d" 
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0a445389c2174ed0b"
-  security_groups = ["sg-0058977174c866ce8"]
+resource "aws_vpc" "myvpc" {
+   tags = {
+     Name = "terra-vpc"
+   }
+   cidr_block = "172.34.0.0/24"
+}
+
+resource "aws_subnet" "subnet1" {
+   tags = {
+      Name = "Terra-subnet1"
+   }
+   cidr_block = "172.34.0.0/25"
+   vpc_id = "${aws_vpc.myvpc.id}"
+}
+
+resource "aws_subnet" "subnet2" {
+   tags = {
+       Name = "Terraform-subnet2"
+   }
+   cidr_block = "172.34.0.128/25"
+   vpc_id = "${aws_vpc.myvpc.id}"
+}
+
+resource "aws_security_group" "mysg" {
+   vpc_id = "${aws_vpc.myvpc.id}"
+   tags = {
+     Name = "Terra-SG"
+   }
+   ingress {
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+   }
+   egress {
+    protocol = "tcp"
+    from_port = 0
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+   }
 }
