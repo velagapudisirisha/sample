89b33741889f6d041bf06da40aad55c2fe15434f
Rajesh Jagarlamudi <jagarlamudirajesh34@gmail.com>
2023-06-15 04:04:35 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,8 @@ resource "aws_route_table" "prt" {
   vpc_id = "${aws_vpc.myvpc.id}" 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.igw.id
+    gateway_id = "${ aws_internet_gateway.igw.id }"
+  }
 }
 
 resource "aws_subnet" "subnet1" {
