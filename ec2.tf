33b42e64503f7bc66528238450135bf2ef3dea84
Rajesh Jagarlamudi <jagarlamudirajesh34@gmail.com>
2023-06-15 04:02:49 +0000

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -10,12 +10,27 @@ resource "aws_vpc" "myvpc" {
    cidr_block = "172.34.0.0/24"
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.myvpc.id}" 
+  tags = {
+    Name = "15june_igw"
+  }
+}
+
+resource "aws_route_table" "prt" {
+  vpc_id = "${aws_vpc.myvpc.id}" 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+}
+
 resource "aws_subnet" "subnet1" {
    tags = {
       Name = "Terra-subnet1"
    }
    cidr_block = "172.34.0.0/25"
    vpc_id = "${aws_vpc.myvpc.id}"
+   map_public_ip_on_launch = true
 }
 
 resource "aws_subnet" "subnet2" {
@@ -24,6 +39,12 @@ resource "aws_subnet" "subnet2" {
    }
    cidr_block = "172.34.0.128/25"
    vpc_id = "${aws_vpc.myvpc.id}"
+   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "psrta" {
+   subnet_id = "${ aws_subnet.subnet1.id }"
+   route_table_id = "${ aws_route_table.prt.id }"
 }
 
 resource "aws_security_group" "mysg" {
